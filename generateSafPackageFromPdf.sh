7422754418b08f12c098bc2983df0fcc57a4e391
Vitor <vitor.silverio.rodrigues@gmail.com>
2015-08-31 16:43:11 -0300

    parametro opcional metadado
    Adiciona o parametro opcional para selecionar o metadado que será utilizado

--- generateSafPackageFromPdf.sh
+++ generateSafPackageFromPdf.sh
@@ -18,8 +18,8 @@
 # Descrição: Script para a criação de diretórios contendo os arquivos 
 # "contents" e "dublin_core.xml" a partir de arquivos PDF coletados para 
 # inclusão no DSpace. 
-# Os nomes dos arquivos PDF devem estar de acordo com o metadado 
-# "dc.identifier.file", presentes nos itens do DSpace.
+# Os nomes dos arquivos PDF devem estar de acordo com o metadado ecolhido
+# ("dc.identifier.file" por padrão), presentes nos itens do DSpace.
 #
 # Uso: "./generateSafPackageFromPdf.sh pdfs out", onde "pdfs" é o diretório 
 # que contém os arquivos PDF coletados e "out" é o diretório de saída.
@@ -27,35 +27,53 @@
 # Author: vitorsilverio
 # Author: jaideraf
 #
-# Last update: 2015-08-24
+# Last update: 2015-08-31
 ###########################################################################
 
+function checkMetadataToUse {
+        if [ -z "$1" ]
+        then
+                metadata="dc.identifier.file"
+        else
+                metadata="$1"
+        fi
+
+        IFS='.' read -a m <<< "$metadata"
+
+        schema="${m[0]}"
+        element="${m[1]}"
+        qualifier="${m[2]}"
+
+}
+
 function makeDirectoryName {
-	printf "item_%05d" "$1"
+        printf "item_%05d" "$1"
 }
 
 function makeContentsFile {
-	printf "%s\tbundle:ORIGINAL" "$1" > "$2"/contents
+        printf "%s\tbundle:ORIGINAL" "$1" > "$2"/contents
 }
 
 function makeDCxmlFile {
-	printf "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>\n<dublin_core schema=\"dc\">\n  <dcvalue element=\"identifier\" qualifier=\"file\" language=\"\">%s</dcvalue>\n</dublin_core>" "$1" > "$2"/dublin_core.xml
+        checkMetadataToUse "$3"
+        printf "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>\n<dublin_core schema=\"$schema\">\n  <dcvalue element=\"$element\" qualifier=\"$qualifier\" language=\"\">%s</dcvalue>\n</dublin_core>" "$1" > "$2"/dublin_core.xml
 }
 
 function main {
-	CONT=1
-	DIR_PDF=$1
-	DIR_OUT=$2
+        CONT=1
+        DIR_PDF=$1
+        DIR_OUT=$2
+        METADATA=$3
 
-	for pdf in $DIR_PDF/*.pdf
-		do
-			PCKG_DIR="$DIR_OUT"/$(makeDirectoryName "$CONT")
-			mkdir "$PCKG_DIR"
-			cp "$pdf" "$PCKG_DIR"
-			makeContentsFile "${pdf/*\//}" "$PCKG_DIR"
-			makeDCxmlFile "${pdf/*\//}" "$PCKG_DIR"
-			CONT=$((CONT+1))
-		done
+        for pdf in $DIR_PDF/*.pdf
+        do
+                PCKG_DIR="$DIR_OUT"/$(makeDirectoryName "$CONT")
+                mkdir "$PCKG_DIR"
+                cp "$pdf" "$PCKG_DIR"
+                makeContentsFile "${pdf/*\//}" "$PCKG_DIR"
+                makeDCxmlFile "${pdf/*\//}" "$PCKG_DIR" "$METADATA"
+                CONT=$((CONT+1))
+        done
 }
 
-main "$1" "$2"
+main "$1" "$2" "$3"
