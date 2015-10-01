33c40eacc3867635658a5a434fe326348cb1336f
Jaider Andrade Ferreira <jaideraf@gmail.com>
2015-09-30 21:04:44 -0300

    Update dedupCsvValues.pl

--- dedupCsvValues.pl
+++ dedupCsvValues.pl
@@ -15,16 +15,18 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 # http://www.gnu.org/copyleft/gpl.html
 #
-# Description : Deduplicate CSV values 
+# Description: Deduplicate CSV values 
+#
+# Syntax: "./dedupCsvValues.pl input_file output_file"
 # 
 # Author: Oberdan Luiz May
 #
 # Last update: 2015-09-10
 # 
-# Requires external libs:
+# External libs required:
 # Text::CSV
 # Tie::IxHash
-# Use CPAN or OS package to install
+# (use CPAN or OS package to install them)
 ###########################################################################
 
 use strict;
@@ -32,46 +34,47 @@ use warnings;
 use Text::CSV;
 use Tie::IxHash;
 
-#Recebe os parametros
+# Recebe os parâmetros
 my ($input_file, $output_file) = @ARGV;
 
-#Inicializa o parser de csv
-my $csv_parser = Text::CSV->new( { binary     => 1, 
-		                   sep_char   => ',', 
-				   quote_char => '"' } );
+# Inicializa o parser de CSV
+my $csv_parser = Text::CSV->new( {	binary     => 1, 
+									sep_char   => ',', 
+									quote_char => '"' } );
 
 open( my $in  , '<:encoding(utf-8)' , $input_file );
 open( my $out , '>:encoding(utf-8)' , $output_file );
 
 while( my $row = $csv_parser->getline($in) ){
-   my $i = 5;
-#   for( my $i = 0 ; $i < 66 ; $i++ ){
-      if( $$row[$i] =~ /\|\|/ ){
-          #Quebra no ||
-	  my @parts = split(/\|\|/,$$row[$i]);
-	  my %count = ();
-          #Os dados ficam na ordem em que entram
-	  tie(%count,'Tie::IxHash');
-          #Conta ocorrencias das partes
-	  foreach my $part (@parts){
-	    $count{$part}++;
-	  };
-          
-	  my $have_dup = 0;
-	  foreach my $part (keys %count){
-	    if( $count{$part} > 1 ){
-	      print "Check $$row[0] -> $i -> $$row[$i]\n";
-	      $have_dup = 1;
-	      last;
-            };
-	  };
+	my $i = 5;
+#	for( my $i = 0 ; $i < 66 ; $i++ ){
+	if( $$row[$i] =~ /\|\|/ ){
+		# Quebra no "||"
+		my @parts = split(/\|\|/,$$row[$i]);
+		my %count = ();
+		# Os dados ficam na ordem em que entram
+		tie(%count,'Tie::IxHash');
+		# Conta ocorrências das partes
+		foreach my $part (@parts){
+			$count{$part}++;
+		};
 
-	 if( $have_dup ){
-            print $out qq/"$$row[0]","/ , join('||',keys(%count)) , qq/"\n/;
-	 };	 
-      };
+		my $have_dup = 0;
+		foreach my $part (keys %count){
+			if( $count{$part} > 1 ){
+				print "Check $$row[0] -> $i -> $$row[$i]\n";
+				$have_dup = 1;
+				last;
+			};
+		};
+
+		if( $have_dup ){
+			print $out qq/"$$row[0]","/ , join('||',keys(%count)) , qq/"\n/;
+		};
+	};
 #   };
 };
+
 $csv_parser->error_diag();
 
 close( $in );
